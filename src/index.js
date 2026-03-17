685bfdcb6e31b6adfa923b9ed39cb4b909bc42b8
Hélias Essabar <helias.essabar@epfedu.fr>
2026-03-17 10:34:36 +0100

    modif index et selection

--- src/index.js
+++ src/index.js
@@ -1,2 +1,38 @@
 import selection from "./js/selection.js"; 
 
+// configuration générale du jeu
+var config = {
+  type: Phaser.AUTO,
+  width: 960, // largeur en pixels
+  height: 960, // hauteur en pixels
+  physics: {
+    // définition des parametres physiques
+    default: "arcade", // mode arcade : le plus simple : des rectangles pour gérer les collisions. Pas de pentes
+    arcade: {
+      debug: false // permet de voir les hitbox et les vecteurs d'acceleration quand mis à true
+    }
+  },
+  scene: [selection]
+};
+
+// création et lancement du jeu
+var game = new Phaser.Game(config);
+game.scene.start("selection");  
+
+
+
+
+var player; // désigne le sprite du joueur
+var clavier;
+var score = 0;
+var zone_texte_score;
+var groupe_bombes;
+var groupe_monstres;
+var gameOver = false;
+
+function chocAvecBombe(un_player, une_bombe) {
+ this.physics.pause();
+ player.setTint(0xff0000);
+ player.anims.play("anim_face");
+ gameOver = true;
+}
--- src/js/selection.js
+++ src/js/selection.js
@@ -1,17 +1,7 @@
 // chargement des librairies
+var player;
 
-/***********************************************************************/
-/** CONFIGURATION GLOBALE DU JEU ET LANCEMENT 
-/***********************************************************************
 
-/***********************************************************************/
-/** FONCTION PRELOAD 
-/***********************************************************************/
-
-/** La fonction preload est appelée une et une seule fois,
- * lors du chargement de la scene dans le jeu.
- * On y trouve surtout le chargement des assets (images, son ..)
- */
 export default class selection extends Phaser.Scene{ 
   constructor() {
     super({key : "selection"});
@@ -106,39 +96,4 @@ export default class selection extends Phaser.Scene{
       return;
     }
   }
-}
-
-// configuration générale du jeu
-var config = {
-  type: Phaser.AUTO,
-  width: 960, // largeur en pixels
-  height: 960, // hauteur en pixels
-  physics: {
-    // définition des parametres physiques
-    default: "arcade", // mode arcade : le plus simple : des rectangles pour gérer les collisions. Pas de pentes
-    arcade: {
-      debug: false // permet de voir les hitbox et les vecteurs d'acceleration quand mis à true
-    }
-  },
-  scene: [selection]
-};
-
-// création et lancement du jeu
-var game = new Phaser.Game(config);
-game.scene.start("selection");  
-
-
-var player; // désigne le sprite du joueur
-var clavier;
-var score = 0;
-var zone_texte_score;
-var groupe_bombes;
-var groupe_monstres;
-var gameOver = false;
-
-function chocAvecBombe(un_player, une_bombe) {
- this.physics.pause();
- player.setTint(0xff0000);
- player.anims.play("anim_face");
- gameOver = true;
 }
