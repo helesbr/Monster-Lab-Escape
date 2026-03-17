1c366d3303ec75a62af13da0b88d3de49cceed9a
Hélias Essabar <helias.essabar@epfedu.fr>
2026-03-17 10:37:10 +0100

    creation map_cuisine

---
+++ src/js/map_cuisine.js
@@ -0,0 +1,11 @@
+export default class map_cuisine extends Phaser.Scene{
+    constructor() {
+        super({key : "map_cuisine"});
+    }
+    preload() {}
+
+    create() {
+        
+    }
+    update() {}
+}
