10eb62b7b83e7ff96ec83d4d400f7ddbf865febd
Arthus <Arthus.bailly@gmail.com>
2026-03-17 10:58:29 +0100

    correction map-stuff map_directeur

---
+++ src/js/map_directeur.js
@@ -0,0 +1,11 @@
+export default class map_directeur extends Phaser.Scene {
+    constructor() {
+        super({ key: "map_directeur" });
+    }
+    preload() { }
+
+    create() {
+
+    }
+    update() { }
+}
---
+++ src/js/map_stuff.js
@@ -0,0 +1,11 @@
+export default class map_stuff extends Phaser.Scene {
+    constructor() {
+        super({ key: "map_stuff" });
+    }
+    preload() { }
+
+    create() {
+
+    }
+    update() { }
+}
