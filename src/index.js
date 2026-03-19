37b730160870f5bd2a37abe769c2bd4d41c174a0
Mehdi  Ammi <Mehdiammi23@gmail.com>
2026-03-19 12:56:32 +0100

    consol

--- src/index.js
+++ src/index.js
@@ -32,5 +32,5 @@ var config = {
 
 // création et lancement du jeu
 var game = new Phaser.Game(config);
-game.scene.start("map_directeur"); // Démarrer avec le menu
+game.scene.start("menu"); // Démarrer avec le menu
 
